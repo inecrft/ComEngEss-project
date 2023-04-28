6d696dc8fe7ce726d1247cc94baa72f8b24293f8
Chantouch Narasetaphan <inecrft4747@gmail.com>
2023-04-28 18:16:18 +0700

    Add somewhat working calendar

---
+++ frontend/script_calendar.js
@@ -0,0 +1,84 @@
+const calendar = document.querySelector(".calendar"),
+  date = document.querySelector(".date"),
+  daysContainer = document.querySelector(".days"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next");
+
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function initCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = months[month] + " " + year;
+
+  let days = "";
+
+  for (let i = day; i > 0; i--) {
+    days += `<div class="day prev-date">${prevDays - i + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDate; i++) {
+    if (
+      i == new Date().getDate() &&
+      year == new Date().getFullYear() &&
+      month == new Date().getMonth()
+    ) {
+      days += `<div class="day today">${i}</div>`;
+    } else {
+      days += `<div class="day">${i}</div>`;
+    }
+  }
+
+  for (let i = 1; i <= nextDays; i++) {
+    days += `<div class="day next-date">${i}</div>`;
+  }
+
+  daysContainer.innerHTML = days;
+}
+
+initCalendar();
+
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  initCalendar();
+}
+
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
