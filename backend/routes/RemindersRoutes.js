6c8f521af0a3c1a4f32b7903bd445f70bdba99fb
junior-meowmeow <121653436+junior-meowmeow@users.noreply.github.com>
2023-05-01 16:12:33 +0700

    Fix reminders routes

--- backend/routes/RemindersRoutes.js
+++ backend/routes/RemindersRoutes.js
@@ -4,7 +4,7 @@ const RemindersController = require("../controller/RemindersController");
 const router = express.Router();
 
 router.post("/users", RemindersController.addNewUser);
-router.get("/", RemindersController.getReminders);
-router.put("/", RemindersController.putReminder);
+router.get("/get", RemindersController.getReminders);
+router.put("/update", RemindersController.putReminder);
 
 module.exports = router;
